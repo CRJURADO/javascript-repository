2c02e40ca6352b0324e9502832c24d72a4633368
CarolinaJurado <carolinajurado.v@gmail.com>
2021-10-21 23:03:19 -0300

    Se agrega y recorre un array de objetos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+//Armado de objeto con su metodo que sirve de funcion principal de la aplicacion
 class Producto{
     constructor(name, price, size){
         this.name = name;
-        this.price = parseFloat(price);
-        this.size = parseInt(size);
+        this.price = price;
+        this.size = size;
     }
 
     totalPrice(quantity) {
@@ -13,26 +14,28 @@ class Producto{
     }
 }
 
-
+//Variables globales
 let name1;
-let drinkSize;
-let foodSize;
-
 let price;
+let size;
 let quantity;
 let totPrice;
+let entryMenu;
 
-const drinkArray = ["Agua", "Gaseosa", "Cerveza", "Mojito", "Vino"];
-const foodArray = ["Hamburguesa completa con papas", "Papas con cheddar", "Nachos con cheddar"];
-let entryMenu = 0;
+//Primer llamado de la aplicacion
 entryMenu = prompt("Ingrese el numero indicado para ingresar al menu que quiere ver 1.Bebidas 2.Comidas");
 viewMenu(entryMenu);
 
+//Ingreso al funcionamiento gral
 if (entryMenu <= 2 && entryMenu > 0) {
-    pickSize(entryMenu);
-    setQty();
-    const producto1 = new Producto(name1, parseFloat(price), parseInt(size));
+
+    pickSize(entryMenu); //eleccion de tamaño
+    setQty();//Eleccion de cantidad
+    alert(name1 + price);//Aviso de nombre y precio del producto unitario
+    //Creacion del objeto producto
+    const producto1 = new Producto(name1, price, size);
     alert(producto1.name + producto1.price);
+    //Calculo del total de la compra y se muestra al usuario con un alert
     alert("Por favor presione aceptar para ingresar a su carrito de compras y pagar");
     producto1.totalPrice(quantity);
     alert("Por favor ingrese su pago de " + totPrice);
@@ -40,19 +43,45 @@ if (entryMenu <= 2 && entryMenu > 0) {
     viewMenu(entryMenu);
 }
 
+//Funciones utilizadas en la aplicacion
 function viewMenu(option) {
     option = parseInt(option);
     switch (option) {
         case 1:
             
+            //Variables locales
+            const drinkArray = ["Agua", "Gaseosa", "Cerveza", "Mojito", "Vino"];
+            const productosDrinks = [{name: "Agua" , price: 250, size: 1},
+                                     {name: "Mojito" , price: 450, size: 1},
+                                     {name: "Gaseosa" , price: 325, size: 1},
+                                     {name: "Vino" , price: 470, size: 1},
+                                     {name: "Cerveza" , price: 400, size: 1}
+                                    ];
+
             alert("Ingresando al menu de Bebidas");
+            
+            let seePrices = prompt("Elija el numero que indique si quiere ver la lista de precios de 1.mas barato a mas caro o 2.de mas caro a mas barato");
+            arrangePrices(seePrices, productosDrinks);
+            alert(JSON.stringify(productosDrinks, "", 2));
+            
             name1 = prompt("Por favor indique el nombre de la bebida que desea.  " + drinkArray.toString());
             return name1;
             break;
     
         case 2:
-        
+            
+            //Variables locales
+            const foodArray = ["Hamburguesa completa con papas", "Papas con cheddar", "Nachos con cheddar"];
+            const productosFoods =  [{name: "Hamburguesa completa con papas", price: 550, size: 1},
+                                     {name: "Papas con cheddar", price: 500, size: 1},
+                                     {name: "Nachos con cheddar", price: 470, size: 1}];
+
             alert("Ingresando al menu de Comidas");
+            
+            let seePrices = prompt("Elija el numero que indique si quiere ver la lista de precios de 1.mas barato a mas caro o 2.de mas caro a mas barato");
+            arrangePrices(seePrices, productosFoods);
+            alert(JSON.stringify(productosFoods, "", 2));
+            
             name1 = prompt("Por favor indique el nombre de comida que desea  " + foodArray.toString());
             return name1;
             break;
@@ -91,7 +120,7 @@ function pickSize(option) {
         
     }else if (option == 2) {
         
-        size = prompt("Por favor ingrese el numero indicador del tamaño 1.Pequeño 2.Mediano 3.Grande");
+        let size = prompt("Por favor ingrese el numero indicador del tamaño 1.Pequeño 2.Mediano 3.Grande");
         size = parseInt(size);
         switch (size) {
             case 1:
@@ -122,4 +151,14 @@ function setQty() {
     return quantity;
     
 }
+
+function arrangePrices(seePrices, products) {
+    
+    if (seePrices == 1) {
+        products.sort((a, b) => a.price - b.price);
+    }else{
+        products.sort((a, b) => b.price - a.price );
+    }
+}
+
 
