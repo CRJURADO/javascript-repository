78053aa24fd5f843b0c73351cbc1b1789c3e78a8
CarolinaJurado <carolinajurado.v@gmail.com>
2021-11-02 23:26:45 -0300

    Se agrega funcionalidad borrar pero no funciona todavia

--- js/main.js
+++ js/main.js
@@ -7,30 +7,31 @@ const productos = [{id:1, nombre:'Agua', descripcion:'Botella de 500ml', precio:
 
 //Funcion para mostrar el menu
 const mostrarMenu = () => {
-for (const producto of productos){
-    let contenedor = document.createElement("div");
-    contenedor.className = "col-sm-4 my-4";
-    contenedor.innerHTML = 
-    `
 
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">${producto.descripcion}</p>
-                <p class="card-text"><strong>$ ${producto.precio}</strong></p>
-                <a href="#" class="btn btn-primary" onclick=prodElegidos(${producto.id})>Comprar</a>
+    for (const producto of productos){
+        let contenedor = document.createElement("div");
+        contenedor.className = "col-sm-4 my-4";
+        contenedor.innerHTML = 
+        `
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.descripcion}</p>
+                    <p class="card-text"><strong>$ ${producto.precio}</strong></p>
+                    <a href="#" class="btn btn-primary" onclick=prodElegidos(${producto.id})>Comprar</a>
+                </div>
             </div>
-        </div>
-             
-    `
-    document.getElementById('fila').appendChild(contenedor);
-    
-}
+                
+        `
+        document.getElementById('fila').appendChild(contenedor);
+        
+    }
 }
 
 //Funcion para incrementar el numero de productos en el carrito
 const prodElegidos = (idProducto) => {
-    console.log(idProducto);
+
     var elemento = document.getElementById('contItm');
     var cantidad = parseFloat(elemento.innerHTML) + 1;
     elemento.innerHTML = cantidad;
@@ -47,7 +48,6 @@ const agregarProd = (idProducto) => {
     let productoComprado = productos.find(x=>x.id ==idProducto);
     carrito.push(productoComprado);
     localStorage.setItem('carrito',JSON.stringify(carrito));
-    alert(`${productoComprado.nombre} se ha agregado correctamente al carrito`);
 }
 
 const verCarrito = () => {
@@ -55,23 +55,50 @@ const verCarrito = () => {
     let compras = JSON.parse(localStorage.getItem('carrito'));
     let totalPrecio = 0;
     let contPrecio = document.createElement("h2");
+    let contCant = 0;
     
     for (const prod of compras){
         let contenedor = document.createElement("li");
         contenedor.className = "list-group-item d-flex justify-content-between align-items-center";
         contenedor.innerHTML = 
         `
-            ${prod.nombre}
+            <div><h4>${prod.nombre}</h4></div>
             <span class="badge bg-primary rounded-pill">$ ${prod.precio}</span>
+            <div class="justify-content-end" id="trash-${prod.id}"><span class="trash"><i class="fas fa-trash"></i></span></div>
 
         `
         totalPrecio += +prod.precio;
+        contCant += +1;
+
+        //La idea era atrapar el ID de cada elemento y poder borrarlo de la lista pero por algun motivo mi dato llega en null cuando hago el getElemetById
+        const trashItem = document.getElementById(`trash-${prod.id}`); //Aca llega en null, porque podria ser?
+
+        /*trashItem.addEventListener("click", () => {
+        deleteItem(prod.id);
+        });*/ 
         
         document.getElementById('listaCarrito').appendChild(contenedor);
     }
+
+    var elemento = document.getElementById('contItm');
+    var cantidad = parseFloat(elemento.innerHTML) + contCant;
+    elemento.innerHTML = cantidad;
+
     contPrecio.innerHTML =
     `
         Total $ ${totalPrecio}
     `
     document.getElementById('total').appendChild(contPrecio);
 }
+
+const deleteItem = (id) => {
+    const option = confirm(
+      "¿Está seguro que quiere eliminar el item seleccionado?"
+    );
+    if (option) {
+      const newCarr = carritoArr.filter((el) => el.id !== id);
+      carritoArr = newCarr;
+      localStorage.setItem('carrito',JSON.stringify(carritoArr));
+      verCarrito(carritoArr);
+    }
+  };
